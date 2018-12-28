5a23e2f064f6bf58732711fb83b591ebecc7aa5d
Nevaan Perera <nevaan9@gmail.com>
2018-12-27 19:26:20 -0500

    added all files to github

---
+++ server/models/user.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+// Define a User Schema
+let UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+// User Schema Methods
+// Override the toJSON method, we send back only relevant information to the client
+// .methods is an instance method
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+
+    // Pick the variables we want to send back
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// Custom method to generate auth token
+UserSchema.methods.genAuthToken = function() {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    // return the token (from the then); this then is returned in the POST /users
+    return user.save()
+        .then(() => {
+            // return the token
+            return token
+        })
+};
+
+// Finds if the passed x-auth token matches; if so, pass the user info
+UserSchema.statics.findByToken = function (token) {
+    // This is the UserSchema; Not a specific user object
+    let User = this;
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+let User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
