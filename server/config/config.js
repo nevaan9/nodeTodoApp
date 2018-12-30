b5225454863e5fd58cd7e6e39c7f88590e19a4c8
Nevaan Perera <nevaan9@gmail.com>
2018-12-30 04:53:42 -0500

    updated configs

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
-var env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    // Load the config file; (This is not included in github)
+    let config = require('./config.json');
+    let configEnv = config[env];
+
+    Object.keys(configEnv).forEach((key) => {
+        process.env[key] = configEnv[key];
+    })
 }
